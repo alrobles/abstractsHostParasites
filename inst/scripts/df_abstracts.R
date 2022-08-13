b859911fb86c4d09cc49c4f445aa927828dc81cd
Angel Luis Robles Fernandez (Student) <alroble8@sundevils.asu.edu>
2022-08-13 08:29:58 -0500

    adding  csv and vignettes to ignored directory. build loadpkg internal function

--- inst/scripts/df_abstracts.R
+++ inst/scripts/df_abstracts.R
@@ -11,23 +11,26 @@ if(require(textcat)){
 
 
 gmpd <- readr::read_csv("data-raw/gmpd_doi_abstract_date.csv") %>%
-  select(doi, title, abstract)
+  select(doi, title, abstract, source)
 zover <- readr::read_csv("data-raw/zover_doi_abstract_date.csv") %>%
-  select(doi, title, abstract)
+  select(doi, title, abstract, source)
 
 df_parasites <- bind_rows(gmpd , zover) %>%
   mutate(class = "parasite") %>%
   mutate(abstract = prep_fun(abstract)) %>%
   mutate(language = textcat(abstract)) %>%
   filter(language == "english")
+df_parasites %>%
+  select(doi, title, abstract, source) %>%
+  write_csv("data-raw/parasiteAbstracts.csv")
+df_parasites <- df_parasites %>% select(doi, title,abstract) %>%
+  mutate(class = "parasite")
 
 df_abstracts_random <- readr::read_csv("data-raw/df_abstracts_random.csv")
 df_abstracts <- bind_rows(df_parasites, df_abstracts_random ) %>%
   sample_frac(1) %>%
-  na.exclude() %>%
-  mutate(abstract = abstractsHostParasites::prep_fun(abstract)) %>%
-  select(-language)
-
+  select(-language) %>%
+  na.exclude()
 
 #write_csv(df_abstracts, "inst/scripts/df_abstracts.csv")
 
